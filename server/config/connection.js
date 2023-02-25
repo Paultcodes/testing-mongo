8c26d60ceb57f20b9020233ce2c846c4de12586e
Paultcodes <paultayloremail1995@gmail.com>
2023-02-25 17:32:59 -0600

    change uri

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //db
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/your-database-name',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/programming-thoughts',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
